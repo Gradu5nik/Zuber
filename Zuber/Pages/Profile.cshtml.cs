ef709955e1df7a40ea6fb0ec37ea744adff44425
Gábor Tamás Tornai <gabo0127@edu.easj.dk>
2021-05-18 09:15:33 +0200

    I changed the profile page and the logo

--- Zuber/Pages/Profile.cshtml.cs
+++ Zuber/Pages/Profile.cshtml.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Zuber.CustomAtributes;
@@ -14,6 +17,9 @@ namespace Zuber.Pages
 {
     public class ProfileModel : PageModel
     {
+        //private readonly Zuber.Pages.Data.ImageContext _context;
+        //private readonly IWebHostEnvironment _iweb;
+
         public IUserService service;
         public SingletonUser User;
         [BindProperty]
@@ -54,5 +60,49 @@ namespace Zuber.Pages
             service.DeleteZuberUser(User.User.Email);
             return RedirectToPage("Logout");
         }
+        
+       //{
+       //     private readonly Zuber.Pages.Data.ImageContext _context;
+       //     private readonly IWebHostEnvironment _iweb;
+
+       //     public ProfileModel(RazorPages_CRUD_Fileupload.Data.ImageContext context, IWebHostEnvironment iweb)
+       //     {
+       //         _context = context;
+       //         _iweb = iweb;
+
+       //     }
+
+       //     public IList<Imagesave> Imagesave { get; set; }
+
+       //     public async Task OnGetAsync()
+       //     {
+       //         Imagesave = await _context.Imagesave.ToListAsync();
+       //     }
+
+       //     public async Task<IActionResult> OnPostAsync(IFormFile uploadfiles, Imagesave img)
+       //     {
+       //         if (!ModelState.IsValid)
+       //         {
+       //             return Page();
+       //         }
+
+       //         string imgext = Path.GetExtension(uploadfiles.FileName);
+       //         if (imgext == ".jpg" || imgext == ".png")
+       //         {
+       //             var imgsave = Path.Combine(_iweb.WebRootPath, "Images", uploadfiles.FileName);
+       //             var stream = new FileStream(imgsave, FileMode.Create);
+       //             await uploadfiles.CopyToAsync(stream);
+       //             stream.Close();
+
+       //             img.Imgname = uploadfiles.FileName;
+       //             img.Imgpath = imgsave;
+       //             await _context.Imagesave.AddAsync(img);
+       //             await _context.SaveChangesAsync();
+       //         }
+
+       //         return RedirectToPage("./Index");
+       //     }
+        //}
     }
 }
+
